b451db71cf40757693659b748cef14ba17c814bb
Jhonnatan Porras <jhonnatanporras23@gmail.com>
2025-06-04 21:17:12 -0500

    'Anthony_ModelPlaya'

--- Aplicaciones/Playa/models.py
+++ Aplicaciones/Playa/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Playa(models.Model):
+    nombre = models.CharField(max_length=100)
+    ubicacion = models.CharField(max_length=200)
+    descripcion = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.nombre
