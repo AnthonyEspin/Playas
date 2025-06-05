48b94fe887ef0b9c42ff6edcebaed126d68b2ad0
Jhonnatan Porras <jhonnatanporras23@gmail.com>
2025-06-04 21:39:09 -0500

    'Anthony_URLSGENERAL'

--- Playas/urls.py
+++ Playas/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,inlcude
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('Aplicaciones.Playa.urls'))
 ]
+if settings.DEBUG: urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
