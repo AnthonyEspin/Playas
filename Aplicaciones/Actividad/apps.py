4006fa33617b804815c919919ff0a70c2b305505
Jhonnatan Porras <jhonnatanporras23@gmail.com>
2025-06-04 21:08:55 -0500

    'Anthony_ConfigurarArchApppyApps'

--- Aplicaciones/Actividad/apps.py
+++ Aplicaciones/Actividad/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ActividadConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Actividad'
+    name = 'Aplicaciones.Actividad'
--- Aplicaciones/Playa/apps.py
+++ Aplicaciones/Playa/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PlayaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Playa'
+    name = 'Aplicaciones.Playa'
