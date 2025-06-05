fdcc93d2a7fed4a735d85673c5ed4ea3edc94533
Jhonnatan Porras <jhonnatanporras23@gmail.com>
2025-06-04 22:59:32 -0500

    'Anthony_InicioActividad'

--- Aplicaciones/Actividad/models.py
+++ Aplicaciones/Actividad/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Actividad(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True)
+    playa = models.ForeignKey(Playa, on_delete=models.CASCADE, related_name='actividades')
+    imagen = models.ImageField(upload_to='imagenes/', blank=True, null=True)
+    archivo = models.FileField(upload_to='archivos/', blank=True, null=True)
+    def __str__(self):
+        return f"{self.nombre} en {self.playa.nombre}"
